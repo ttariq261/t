f361b6e801c0e5fa007dbe32907db400c73703ef
tariq <ttariq261@gmail.com>
2014-03-10 10:37:09 +0545

    adjusting images

--- js/services.js
+++ js/services.js
@@ -8,10 +8,10 @@ angular.module('starter.services', [])
 
   // Some fake testing data
   var pets = [
-    { id: 0, title: 'Cats', description: 'Furry little creatures. Obsessed with plotting assassination, but never following through on it.' },
-    { id: 1, title: 'Dogs', description: 'Lovable. Loyal almost to a fault. Smarter than they let on.' },
-    { id: 2, title: 'Turtles', description: 'Everyone likes turtles.' },
-    { id: 3, title: 'Sharks', description: 'An advanced pet. Needs millions of gallons of salt water. Will happily eat you.' }
+    { id: 0, title: 'Logo for a Morrocan Restaurant', description: 'Furry little creatures. Obsessed with plotting assassination, but never following through on it.' },
+    { id: 1, title: 'Going to the movies?!', description: 'Lovable. Loyal almost to a fault. Smarter than they let on.' },
+    { id: 2, title: 'McDonalds Free Offer', description: 'Everyone likes turtles.' },
+    { id: 3, title: 'University for Actuarial Science', description: 'An advanced pet. Needs millions of gallons of salt water. Will happily eat you.' }
   ];
 
   return {

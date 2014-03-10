ff374894df6c2c2965dff706d85928dc56dd4953
tariq <ttariq261@gmail.com>
2014-03-10 23:50:44 +0545

    statereload

--- js/controllers.js
+++ js/controllers.js
@@ -2,12 +2,14 @@ angular.module('starter.controllers', [])
 
 
 // A simple controller that fetches a list of data from a service
-.controller('PetIndexCtrl', function($scope, PetService) {
+.controller('PetIndexCtrl', function($scope, $state) {
   // "Pets" is a service returning mock data (services.js)
-  $scope.pets = PetService.all();
+
   	$scope.tickChoice = function(){
   		var element = document.getElementById('tick');
   		element.style.display = 'block';
+  	setTimeout(function(){$state.go($state.$current, null, { reload: true });},500);
+  		
   	}
 })
 
